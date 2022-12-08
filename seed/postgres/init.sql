2555211e98d9af474fadc1e0843269f6bff41846
dae-ne <rafalcz29@gmail.com>
2022-12-08 21:15:30 +0100

    docker-compose db setup with seed data

---
+++ seed/postgres/init.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS fdc;
+
+CREATE DATABASE fdc;
+
+CREATE TABLE food (
+   fdc_id INT PRIMARY KEY,
+   data_type TEXT,
+   description TEXT,
+   publication_date DATE
+);
+
+CREATE TABLE branded_food (
+   fdc_id INT PRIMARY KEY REFERENCES food,
+   brand_owner TEXT,
+   gtin_upc TEXT,
+   ingredients TEXT,
+   serving_size INT,
+   serving_size_unit TEXT,
+   branded_food_category TEXT,
+   data_source TEXT,
+   modified_date DATE,
+   available_date DATE,
+   market_country TEXT,
+   trade_channel TEXT
+);
