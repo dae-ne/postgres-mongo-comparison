7c96ab4d298ef9bc59ef4bc102d48dd24afe65e7
dae-ne <rafalcz29@gmail.com>
2023-01-04 01:52:26 +0100

    Update the logger middleware

--- api/middlewares.ts
+++ api/middlewares.ts
@@ -3,7 +3,9 @@ import { logger } from './library/logging';
 
 export const loggerMiddleware = (req: Request, _: Response, next: NextFunction) => {
   const { method, url } = req;
-  const message = `request: ${method} ${url}`;
-  logger.info(message);
+  logger.info(`request: ${method} ${url}`);
+  const start = Date.now();
   next();
+  const elapsed = Date.now() - start;
+  logger.info(`done: ${method} ${url} (time: ${elapsed} ms)`);
 };
