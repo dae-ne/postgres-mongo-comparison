9a962efefac4329a603de9d754047e5e79a7407b
dae-ne <rafalcz29@gmail.com>
2023-01-09 01:31:03 +0100

    Update time in logger

--- api/library/logging.ts
+++ api/library/logging.ts
@@ -6,20 +6,10 @@ const COLOR_RED = '\x1b[31m';
 const COLOR_LIGHT_BLUE = '\x1b[94m';
 
 const PREFIX_PAD_LENGTH = 18;
-const DATE_TIME_PAD_LENGTH = 21;
-
-// TODO: there's probably a better way to do that
-const twoZerosPad = (value: number | string) => `${value}`.padStart(2, '0');
-
-const formatDate = (date: Date) =>
-  `${date.toLocaleDateString()} ${twoZerosPad(date.getHours())}:${twoZerosPad(
-    date.getMinutes()
-  )}:${twoZerosPad(date.getSeconds())}`;
 
 const getMessageWithDateTime = (...message: (string | number)[]) => {
   const date = new Date();
-  const formattedDate = `[${formatDate(date)} UTC]`.padEnd(DATE_TIME_PAD_LENGTH);
-  return `${formattedDate} ${message.join(' ')}`;
+  return `[${date.toLocaleTimeString()}] ${message.join(' ')}`;
 };
 
 const logInfo = (...message: (string | number)[]) =>
